d080312fe525d66e7042cdc659ca82a00e686631
waldoeGeek <waldoe@archlinux.info>
2018-11-07 12:10:47 -0500

    added function to output low inventory items

--- menu.cpp
+++ menu.cpp
@@ -62,6 +62,7 @@ void addData() {
    case 'k': sqlString = "INSERT INTO kitchen (itemID, itemName, itemPrice, itemInv) VALUES(itemId," + itemName + "," + itemPrice + "," + itemInv + ")"; break;
    case 'a': sqlString = "INSERT INTO bathroom (itemID, itemName, itemPrice, itemInv) VALUES(itemId," + itemName + "," + itemPrice + "," + itemInv + ")"; break;
    case 'l': sqlString = "INSERT INTO livingroom (itemID, itemName, itemPrice, itemInv) VALUES(itemId," + itemName + "," + itemPrice + "," + itemInv + ")"; break;
+   case 'o': sqlString = "INSERT INTO office (itemID, itemName, itemPrice, itemInv) VALUES(itemId," + itemName + "," + itemPrice + "," + itemInv + ")"; break;
    default : cout << "Make a selection:";
  }
 
@@ -96,7 +97,7 @@ void dispData() {
   string sqlString;
   char choice;
   cout << "Which Category? " << endl <<
-      "(B)edroom, (K)itchen, b(A)throom, (L)ivingRoom" << endl;
+      "(B)edroom, (K)itchen, b(A)throom, (L)ivingRoom (O)ffice" << endl;
 
   cin >> choice;
 
@@ -106,6 +107,7 @@ void dispData() {
     case 'k': sqlString = "SELECT * FROM kitchen;"; break;
     case 'a': sqlString = "SELECT * FROM bathroom;"; break;
     case 'l': sqlString = "SELECT * FROM livingroom;"; break;
+    case 'o': sqlString = "SELECT * FROM office;"; break;
     default : cout << "Make a selection:";
   }
  // Code to ask user a Category
@@ -113,10 +115,11 @@ void dispData() {
   // convert char
   const char *newString = sqlString.c_str();
 
-  // Dumo database rows:
+  // Dump database rows:
   if (mysql_query(con, newString)) {
     finish_with_errors(con);
  }
+
 
   MYSQL_RES *result = mysql_store_result(con);
 
@@ -137,26 +140,95 @@ void dispData() {
     cout << " " << endl;
 }
 
-// function to search data
-void searchData() {
-  // code //
+// function to search low inventory
+void checkInv() {
+
+    MYSQL *con = mysql_init(NULL);
+
+    if (con == NULL) {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      exit(1);
+    }
+
+    if (mysql_real_connect(con, "localhost", "team", "qwerty", "testdb", 0, NULL, 0) == NULL) {
+      finish_with_errors(con);
+   }
+
+   // Code to ask user a Category
+    string sqlString;
+    char choice;
+    cout << "Which Category? " << endl <<
+        "(B)edroom, (K)itchen, b(A)throom, (L)ivingRoom (O)ffice" << endl;
+
+    cin >> choice;
+    cin.ignore(256, '\n');
+
+    // Add appropriate sql depending on category chosen
+    switch (choice) {
+      case 'b': sqlString = "SELECT * FROM bedroom WHERE itemInv < 5;"; break;
+      case 'k': sqlString = "SELECT * FROM kitchen WHERE itemInv < 5;"; break;
+      case 'a': sqlString = "SELECT * FROM bathroom WHERE itemInv < 5;"; break;
+      case 'l': sqlString = "SELECT * FROM livingroom WHERE itemInv < 5;"; break;
+      case 'o': sqlString = "SELECT * FROM office WHERE itemInv < 15;"; break;
+      default : cout << "Make a selection:";
+    }
+
+    // convert char
+    const char *newString = sqlString.c_str();
+
+    cout << "You are Low on...." << '\n';
+    // Dump database rows:
+    if (mysql_query(con, newString)) {
+      finish_with_errors(con);
+   }
+
+    MYSQL_RES *result = mysql_store_result(con);
+
+    int num_fields = mysql_num_fields(result);
+
+    MYSQL_ROW row;
+
+    while ((row = mysql_fetch_row(result))) {
+      for (int i = 0; i < num_fields; i++) {
+        printf("%s ", row[i] ? row[i] : "NULL" );
+
+      }
+      printf("\n" );
+
+    }
+
+    mysql_free_result(result);
+    mysql_close(con);
+    cout << " " << endl;
+
+    // Dummy code to appear as to have ordered more....
+    char answer;
+    string ans;
+    int amount;
+    cout << '\n' << "Order More? " << '\n';
+    cout << "(Y)es, (N)o " << '\n';
+    cin >> answer;
+    cin.ignore(256, '\n');
+    if (answer == 'y') {
+      cout << "Order more of: " << '\n';
+      cin >> ans;
+      cin.ignore(256, '\n');
+      cout << "Quantity? " << '\n';
+      cin >> amount;
+      cin.ignore(256, '\n');
+      cout << "You Have ordered " << amount << " more " << ans << '\n';
+    }
+
+
 }
 
-// function to switch table
-void openTable() {
-  cout << "message" << '\n';
-}
-
-void sqlInsert() {
-  cout << "Item Category?";
-};
 
 // menu function
 char getMenuResponse() {
 
   char response;
 	cout << endl << "Pick Category: " << endl
-		 << "(A)dd Item, (S)earh, (C)hange Category, (Q)uit" << endl
+		 << "(A)dd Item, (S)earh, (I)nventory Check (Q)uit" << endl
 		 << "> ";
 	cin >> response;
 	cin.ignore(256, '\n');
@@ -176,8 +248,7 @@ int main( int argc, char *argv[] ) {
   	{
 	    case 'A': addData(); break;
   		case 'S': dispData(); break;
-  		case 'L': openTable(); break;
-  		case 'E': searchData(); break;
+  		case 'I': checkInv(); break;
   		case 'Q': run = false; break;
   		default : cout << "That is NOT a valid choice" << endl;
   	}
